740ad4853ad83de7bf31b1348b93d084324df573
helloworld654 <zhai.shiqiang@163.com>
2022-04-23 10:17:42 +0800

    [pwm] add more pwm channel to 4 pwm

--- hello_world/main/mcpwm_brushed_dc_control_example.c
+++ hello_world/main/mcpwm_brushed_dc_control_example.c
@@ -22,14 +22,20 @@
 #include "driver/mcpwm.h"
 #include "soc/mcpwm_periph.h"
 
-#define GPIO_PWM0A_OUT 15   //Set GPIO 15 as PWM0A
-#define GPIO_PWM0B_OUT 16   //Set GPIO 16 as PWM0B
+#define GPIO_PWM0A_OUT 19   //Set GPIO 19 as PWM0A
+#define GPIO_PWM0B_OUT 18   //Set GPIO 18 as PWM0B
+#define GPIO_PWM1A_OUT 15   //Set GPIO 15 as PWM1A
+#define GPIO_PWM1B_OUT 14   //Set GPIO 14 as PWM1B
+// #define GPIO_PWM2A_OUT 17   //Set GPIO 17(UART2-TX) as PWM2A
+// #define GPIO_PWM2B_OUT 16   //Set GPIO 16(UART2-RX) as PWM2B
 
 static void mcpwm_example_gpio_initialize(void)
 {
     printf("initializing mcpwm gpio...\n");
     mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0A, GPIO_PWM0A_OUT);
     mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0B, GPIO_PWM0B_OUT);
+    mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM1A, GPIO_PWM1A_OUT);
+    mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM1B, GPIO_PWM1B_OUT);
 }
 
 /**
@@ -61,6 +67,27 @@ static void brushed_motor_stop(mcpwm_unit_t mcpwm_num, mcpwm_timer_t timer_num)
     mcpwm_set_signal_low(mcpwm_num, timer_num, MCPWM_OPR_B);
 }
 
+void pwm_example_config(void)
+{
+    //2. initial mcpwm configuration
+    printf("Configuring Initial Parameters of mcpwm...\n");
+    mcpwm_config_t pwm_config;
+    
+    pwm_config.frequency = 1000;    //frequency = 1000Hz,
+    pwm_config.cmpr_a = 0;    //duty cycle of PWMxA = 0
+    pwm_config.cmpr_b = 0;    //duty cycle of PWMxb = 0
+    pwm_config.counter_mode = MCPWM_UP_COUNTER;
+    pwm_config.duty_mode = MCPWM_DUTY_MODE_0;
+    mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_0, &pwm_config);    //Configure PWM0A & PWM0B with above settings
+
+    pwm_config.frequency = 1000;    //frequency = 1000Hz,
+    pwm_config.cmpr_a = 0;    //duty cycle of PWMxA = 0
+    pwm_config.cmpr_b = 0;    //duty cycle of PWMxb = 0
+    pwm_config.counter_mode = MCPWM_UP_COUNTER;
+    pwm_config.duty_mode = MCPWM_DUTY_MODE_0;
+    mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_1, &pwm_config);    //Configure PWM1A & PWM1B with above settings
+}
+
 /**
  * @brief Configure MCPWM module for brushed dc motor
  */
@@ -68,23 +95,17 @@ static void mcpwm_example_brushed_motor_control(void *arg)
 {
     //1. mcpwm gpio initialization
     mcpwm_example_gpio_initialize();
-
-    //2. initial mcpwm configuration
-    printf("Configuring Initial Parameters of mcpwm...\n");
-    mcpwm_config_t pwm_config;
-    pwm_config.frequency = 1000;    //frequency = 500Hz,
-    pwm_config.cmpr_a = 0;    //duty cycle of PWMxA = 0
-    pwm_config.cmpr_b = 0;    //duty cycle of PWMxb = 0
-    pwm_config.counter_mode = MCPWM_UP_COUNTER;
-    pwm_config.duty_mode = MCPWM_DUTY_MODE_0;
-    mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_0, &pwm_config);    //Configure PWM0A & PWM0B with above settings
+    pwm_example_config();
     while (1) {
         brushed_motor_forward(MCPWM_UNIT_0, MCPWM_TIMER_0, 50.0);
-        vTaskDelay(2000 / portTICK_RATE_MS);
+        brushed_motor_forward(MCPWM_UNIT_0, MCPWM_TIMER_1, 60.0);
+        vTaskDelay(1000 / portTICK_RATE_MS);
         brushed_motor_backward(MCPWM_UNIT_0, MCPWM_TIMER_0, 30.0);
-        vTaskDelay(2000 / portTICK_RATE_MS);
+        brushed_motor_backward(MCPWM_UNIT_0, MCPWM_TIMER_1, 40.0);
+        vTaskDelay(1000 / portTICK_RATE_MS);
         brushed_motor_stop(MCPWM_UNIT_0, MCPWM_TIMER_0);
-        vTaskDelay(2000 / portTICK_RATE_MS);
+        brushed_motor_stop(MCPWM_UNIT_0, MCPWM_TIMER_1);
+        vTaskDelay(1000 / portTICK_RATE_MS);
     }
 }
 
