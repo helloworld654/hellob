8ae3973ea26d532e95ddd238a506d1f4cad58b9b
helloworld654 <zhai.shiqiang@163.com>
2022-06-25 15:11:43 +0800

    add queue use example function

--- hello_world/main/freertos_study.c
+++ hello_world/main/freertos_study.c
@@ -3,23 +3,51 @@
 #include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/queue.h"
 #include "freertos_study.h"
 
 #define RTOS_TASK_STACK_SIZE    2048
 #define RTOS_TASK_PRIORITY    1
+#define RTOS_TEST_QUEUE_LEN    0x10
 
-void *rtos_study_task_handle;
+void *rtos_study_task_handle_A;
+void *rtos_study_task_handle_B;
+void *rtos_study_queue_handle;
 
-static void rtos_study_task(void *arg)
+static void rtos_study_task_a(void *arg)
 {
+    queue_item item_send;
+    printf("[%s] enter\r\n",__func__);
+    rtos_study_queue_handle = xQueueCreate(RTOS_TEST_QUEUE_LEN,sizeof(queue_item));
+    if(!rtos_study_queue_handle){
+        printf("create queue fail\r\n");
+    }
+    while(1){
+        item_send.type = 0xa;
+        printf("[%s] send item to queue\r\n",__func__);
+        xQueueSend(rtos_study_queue_handle,&item_send,0x1000);
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+    }
+}
+
+static void rtos_study_task_b(void *arg)
+{
+    queue_item item_recv;
     printf("[%s] enter\r\n",__func__);
     while(1){
-        printf("[%s] every cycle\r\n",__func__);
-        vTaskDelay(1500/portTICK_PERIOD_MS);
+        if(rtos_study_queue_handle){
+            if(xQueueReceive(rtos_study_queue_handle,&item_recv,1000)){
+                printf("[%s] receive item:0x%x\r\n",__func__,item_recv.type);
+            }
+            else{
+                printf("receive from queue timeout\r\n");
+            }
+        }
     }
 }
 
 void task_create_test(void)
 {
-    xTaskCreate(rtos_study_task,"study_task",RTOS_TASK_STACK_SIZE,NULL,RTOS_TASK_PRIORITY,rtos_study_task_handle);  // stack size should more than 2048
+    xTaskCreate(rtos_study_task_a,"study_task_A",RTOS_TASK_STACK_SIZE,NULL,RTOS_TASK_PRIORITY,rtos_study_task_handle_A);  // stack size should more than 2048
+    xTaskCreate(rtos_study_task_b,"study_task_B",RTOS_TASK_STACK_SIZE,NULL,RTOS_TASK_PRIORITY,rtos_study_task_handle_B);
 }
--- hello_world/main/freertos_study.h
+++ hello_world/main/freertos_study.h
@@ -1,6 +1,11 @@
 #ifndef __FREERTOS_STUDY_H__
 #define __FREERTOS_STUDY_H__
 
+typedef struct{
+    uint8_t type;
+    void *p_data;
+}queue_item;
+
 void task_create_test(void);
 
 #endif
