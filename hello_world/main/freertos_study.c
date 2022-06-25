4b9af91d07c4aae68af3453a1c13550f2a76fee7
helloworld654 <zhai.shiqiang@163.com>
2022-06-25 16:12:13 +0800

    [freertos] add sem example function

--- hello_world/main/freertos_study.c
+++ hello_world/main/freertos_study.c
@@ -4,37 +4,59 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/queue.h"
+#include "freertos/semphr.h"
 #include "freertos_study.h"
 
 #define RTOS_TASK_STACK_SIZE    2048
 #define RTOS_TASK_PRIORITY    1
-#define RTOS_TEST_QUEUE_LEN    0x10
 
 void *rtos_study_task_handle_A;
 void *rtos_study_task_handle_B;
+
+#if defined(QUEUE_TEST) && QUEUE_TEST
+#define RTOS_TEST_QUEUE_LEN    0x10
 void *rtos_study_queue_handle;
+#endif
+
+#if defined(SEM_TEST) && SEM_TEST
+void *rtos_study_sem_handle;
+#endif
 
 static void rtos_study_task_a(void *arg)
 {
-    queue_item item_send;
     printf("[%s] enter\r\n",__func__);
+#if defined(QUEUE_TEST) && QUEUE_TEST
+    queue_item item_send;
     rtos_study_queue_handle = xQueueCreate(RTOS_TEST_QUEUE_LEN,sizeof(queue_item));
-    if(!rtos_study_queue_handle){
-        printf("create queue fail\r\n");
-    }
+#endif
+
+#if defined(SEM_TEST) && SEM_TEST
+    rtos_study_sem_handle = xSemaphoreCreateCounting(1,0);
+#endif
+
     while(1){
+#if defined(QUEUE_TEST) && QUEUE_TEST
         item_send.type = 0xa;
         printf("[%s] send item to queue\r\n",__func__);
         xQueueSend(rtos_study_queue_handle,&item_send,0x1000);
+#endif
+
+#if defined(SEM_TEST) && SEM_TEST
+        printf("[%s] give the sem\r\n",__func__);
+        xSemaphoreGive(rtos_study_sem_handle);
+#endif
         vTaskDelay(1000/portTICK_PERIOD_MS);
     }
 }
 
 static void rtos_study_task_b(void *arg)
 {
-    queue_item item_recv;
     printf("[%s] enter\r\n",__func__);
+#if defined(QUEUE_TEST) && QUEUE_TEST
+    queue_item item_recv;
+#endif
     while(1){
+#if defined(QUEUE_TEST) && QUEUE_TEST
         if(rtos_study_queue_handle){
             if(xQueueReceive(rtos_study_queue_handle,&item_recv,1000)){
                 printf("[%s] receive item:0x%x\r\n",__func__,item_recv.type);
@@ -43,6 +65,16 @@ static void rtos_study_task_b(void *arg)
                 printf("receive from queue timeout\r\n");
             }
         }
+#endif
+
+#if defined(SEM_TEST) && SEM_TEST
+        if(rtos_study_sem_handle){
+            if(xSemaphoreTake(rtos_study_sem_handle,1000)){
+                printf("[%s] take sem success\r\n",__func__);
+            }
+        }
+#endif
+        vTaskDelay(10/portTICK_PERIOD_MS);
     }
 }
 
--- hello_world/main/freertos_study.h
+++ hello_world/main/freertos_study.h
@@ -1,6 +1,9 @@
 #ifndef __FREERTOS_STUDY_H__
 #define __FREERTOS_STUDY_H__
 
+#define QUEUE_TEST    0
+#define SEM_TEST    0
+
 typedef struct{
     uint8_t type;
     void *p_data;
