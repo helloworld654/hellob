54d59f18745875c1208c33d987fa944807ef3a1e
hello <zhai.shiqiang@163.com>
2023-03-31 22:04:44 +0800

    remove the unuseful files

--- hello_world/main/i2c_example_main.h
+++
@@ -1,8 +0,0 @@
-#ifndef __I2C_EXAMPLE_MAIN_H__
-#define __I2C_EXAMPLE_MAIN_H__
-
-uint8_t i2c_write_sensor_reg(uint8_t reg_addr, uint8_t reg_data);
-
-uint8_t i2c_read_sensor_reg(uint8_t reg_addr,uint8_t *data_rd, size_t size);
-
-#endif
--- hello_world/main/motor_control/dc_motor.h
+++
@@ -1,4 +0,0 @@
-#ifndef __DC_MOTOR_H__
-#define __DC_MOTOR_H__
-
-#endif
